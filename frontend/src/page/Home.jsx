b65dc0adbd8cffd7f9ee884f27b0741248ac854a
arbiterkdh <arbiterkdh@gmail.com>
2024-06-03 19:09:32 +0900

    movie, book, theater, promotion, store 폴더, 컴포넌트 생성

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,10 +1,12 @@
-import {Box} from "@chakra-ui/react";
-import {Navbar} from "../component/Navbar.jsx"
-import {Outlet} from "react-router-dom";
+import { Box } from "@chakra-ui/react";
+import { Navbar } from "../component/Navbar.jsx";
+import { Outlet } from "react-router-dom";
 
 export function Home() {
-  return <Box>
-    <Navbar />
-    <Outlet />
-  </Box>;
+  return (
+    <Box>
+      <Navbar />
+      <Outlet />
+    </Box>
+  );
 }
