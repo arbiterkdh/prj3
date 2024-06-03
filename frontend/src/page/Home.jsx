9582f2f4eed007ea175d062e9ba39f71af293e6b
arbiterkdh <arbiterkdh@gmail.com>
2024-06-03 18:12:27 +0900

    frontend: Home.jsx 에 Outlet 작성
    backend: AppConfiguration.java 기본 security 설정,
    각 컴포넌트 별 controller, service, mapper 생성.

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,8 +1,12 @@
-import {Box} from "@chakra-ui/react";
-import {Navbar} from "../component/Navbar.jsx"
+import { Box } from "@chakra-ui/react";
+import { Navbar } from "../component/Navbar.jsx";
+import { Outlet } from "react-router-dom";
 
 export function Home() {
-  return <Box>
-    <Navbar />
-  </Box>;
+  return (
+    <Box>
+      <Navbar />
+      <Outlet />
+    </Box>
+  );
 }
