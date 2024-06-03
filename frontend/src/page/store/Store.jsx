e187fb2a0a64fa7f70d5a3e831ac887fb78aa9dd
gyeyoung7962 <gaeyoung7962@naver.com>
2024-06-03 18:13:49 +0900

    store add 페이지 작성중

--- frontend/src/page/store/Store.jsx
+++ frontend/src/page/store/Store.jsx
@@ -1,3 +1,21 @@
+import { Box } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
+
 export function Store() {
-  return null;
+  return (
+    <Box
+      mx={{
+        base: 0,
+        lg: 500,
+      }}
+      my={{
+        base: 0,
+        lg: 200,
+      }}
+    >
+      {/*<Heading>스토어 페이지입니다</Heading>*/}
+      {/*<hr />*/}
+      <Outlet />
+    </Box>
+  );
 }
