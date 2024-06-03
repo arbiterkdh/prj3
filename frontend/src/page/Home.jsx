32c218915be7fb8f907feb5aac14ef320d80cda2
parksg <park0110725@naver.com>
2024-06-03 18:33:05 +0900

    ㅇㅇ

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,8 +1,10 @@
 import {Box} from "@chakra-ui/react";
 import {Navbar} from "../component/Navbar.jsx"
+import {Outlet} from "react-router-dom";
 
 export function Home() {
   return <Box>
     <Navbar />
+    <Outlet />
   </Box>;
 }
