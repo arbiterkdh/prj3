2b066e35c4f05395b4ba77e73fb1e042e8c3c9e2
parksg <park0110725@naver.com>
2024-06-04 16:14:40 +0900

    promo promoList, promoAdd 작성 및 수정

--- frontend/src/page/movie/MovieList.jsx
+++ frontend/src/page/movie/MovieList.jsx
@@ -1,3 +1,3 @@
 export function MovieList() {
-  return null;
+  return;
 }
--- frontend/src/page/promotion/PromoList.jsx
+++ frontend/src/page/promotion/PromoList.jsx
@@ -1,6 +1,82 @@
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
 
 export function PromoList() {
-  let navigate = useNavigate();
-  return ;
+  const navigate = useNavigate();
+  return (
+    <Box>
+      <SimpleGrid
+        spacing={3}
+        templateColumns="repeat(auto-fill, minmax(250px, 1fr))"
+      >
+        <Card>
+          <CardBody>
+            <Image
+              boxSize="100%"
+              src="https://img.cgv.co.kr/WebApp/contents/eventV4/40714/17171440149400.jpg"
+            />
+            <Text as="b">이벤트 제목</Text>
+            <Text>2024.05.01~2024.05.20</Text>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardBody>
+            <Image
+              boxSize="100%"
+              src="https://img.megabox.co.kr/SharedImg/event/2024/06/03/72bX99ejH4ApMb9eVO25CoLefKRRtqtm.jpg"
+            />
+            <Text as="b">text2</Text>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardBody>
+            <Image boxSize="100%" src="https://bit.ly/dan-abramov" />
+            <Text as="b">text3</Text>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardBody>
+            <Image
+              boxSize="100%"
+              src="https://img.megabox.co.kr/SharedImg/event/2024/05/31/HjP5mCtbMmvHLJriWGIMgYFaKApz3sss.jpg"
+            />
+            <Text as="b">text4</Text>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardBody>
+            <Image
+              boxSize="100%"
+              src="https://img.megabox.co.kr/SharedImg/event/2024/05/31/3J2kBBPmsWIPmFe8mrjAMUilvUGKai6n.jpg"
+            />
+            <Text as="b">text5</Text>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+      </SimpleGrid>
+    </Box>
+  );
 }
