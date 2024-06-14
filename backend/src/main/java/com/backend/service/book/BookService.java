14a92e774ed2b58e09f9beff0709210feb476657
arbiterkdh <arbiterkdh@gmail.com>
2024-06-14 21:27:17 +0900

    수정수정 수정

--- backend/src/main/java/com/backend/service/book/BookService.java
+++ backend/src/main/java/com/backend/service/book/BookService.java
@@ -60,18 +60,6 @@ public class BookService {
 
     public Integer getDayOfOneWeekAgo() {
         return bookMapper.selectDayOfOneWeekAgo();
-
-//        return switch (dayOfOneWeekAgoNumber) {
-//            case 1 -> "일";
-//            case 2 -> "월";
-//            case 3 -> "화";
-//            case 4 -> "수";
-//            case 5 -> "목";
-//            case 6 -> "금";
-//            case 7 -> "토";
-//            default -> "";
-//        };
-
     }
 
     public List<LocalDate> getBookPeriodList() {
